1c09f2e643de5ac7ca3b5b9d3887c2a4cc16ad6a
farah-halim <level2bis2@gmail.com>
2024-08-23 01:10:23 +0300

    task 1

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""
-This method checks a data set to determine if
-it is a valid data set
-
-"""
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
-    """
-    function that checks the utf-8 validity of
-    a dataset, which is passed as an argument.
-    This function returns true if the data is a
-    valid utf-8 encoding, else it returns false.
-
-    """
-
-    try:
-        bytes(data).decode('utf-8')
-        return True
-    except (ValueError, UnicodeDecodeError):
-        return False
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    n_bytes = 0
+    for num in data:
+        bin_rep = format(num, '#010b')[-8:]
+        if n_bytes == 0:
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+                return False
+        n_bytes -= 1
+    return n_bytes == 0
